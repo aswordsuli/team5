f5f49d5da88f470c124cab1566d0b01ea7a41a30
ahmad <aswordsuli@gmail.com>
2023-08-22 16:11:05 +0300

    main function ver1

--- staam.sh
+++ staam.sh
@@ -1,22 +1,35 @@
 #!/bin/bash 
 
+menu () {
+    echo a. Input a Series. 
+    echo b. Display the series in the order it was entered.
+    echo c. Display the series in sorted order 
+    echo d. Display the Max value of the series.
+    echo e. Display the Min value of the series.
+    echo f. Display the Average of the series.
+    echo g. Display the Number of elements in the series.
+    echo h. Display the Sum of the series.
+    echo i. Exit.
+    echo "choose from (a/b/c/d/e/f/g/h/i)"
+}
 
 
+main(){
 
+  if [[ $# -ge 3 ]] && validate_numbers "$@"  ; then 
+    numbers=("$@") 
+    else 
+    echo "wrong input"
+   #ask for input numbers
+    fi 
+  while true ; do
+    menu 
+    read choice
+    case $choise in 
 
+    esac
 
+  done
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-################################################################
-echo "ahmad"
+main $@
